a77bbe833fea453605ae6703de044a8aef0c31ec
Paul Mairot <72132015+PaulMairot@users.noreply.github.com>
2023-01-13 17:08:24 +0100

    Update config.js

--- config.js
+++ config.js
@@ -14,6 +14,7 @@ const root = __dirname;
 const configFromEnvironment = {
   baseUrl: process.env.BASE_URL || process.env.RENDER_EXTERNAL_URL,
   bcryptCost: parseEnvInt('BCRYPT_COST'),
+  cors: parseEnvBoolean('CORS'),
   db: process.env.DATABASE_URI || process.env.DATABASE_URL || process.env.MONGODB_URI || process.env.MONGODB_URL,
   docs: {
     browser: process.env.DOCS_BROWSER,
