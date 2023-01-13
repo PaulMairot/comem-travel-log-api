a232783a46b94cb1d726659f8e049109f358436c
Paul Mairot <72132015+PaulMairot@users.noreply.github.com>
2023-01-13 18:19:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:8100");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
